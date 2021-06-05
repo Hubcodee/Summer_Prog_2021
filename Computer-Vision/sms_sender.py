f57659458c967fc7c5f99d8aaea308b4584687a6
Ansh Shrivas <anshshrivas26@gmail.com>
2021-06-05 20:59:20 +0530

    sms_sender using twilio api

--- Computer-Vision/sms_sender.py
+++ Computer-Vision/sms_sender.py
@@ -1,42 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[11]:
-
-
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
 
 import datetime
 date = datetime.datetime.now() 
 def sendsms():
-    account_sid = "ACdcfae8e5c4069d3cb80385ce2d36c855"
-    auth_token = "ccef4133311775065451e4b028c7d296"
+    account_sid = "########"
+    auth_token = "########"
     client = Client(account_sid, auth_token)
 
     message = client.messages                     .create(
                          body=f"Face detected at {date}",
-                         from_='+12407248231',
-                         to='+91 7350365576'
+                         from_='#########',
+                         to='#########'
                      )
 
     print(f"Messaged successfully !!")
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
