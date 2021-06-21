457882f6cc76682b3b418802b6ea91194ab7e82d
Ansh Shrivas <anshshrivas26@gmail.com>
2021-06-21 22:46:49 +0530

    Fcae detection - automation and integration

---
+++ Computer-Vision/Face Detection/whatsapp_send.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pywhatkit
+def send_what(date):
+    message=f"face detected at {date}"
+    pywhatkit.sendwhatmsg_instantly("+917350365576",message)
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
