e06ec3ee916d1e583980b4e4dec82982f93c2606
Ansh Shrivas <anshshrivas26@gmail.com>
2021-06-21 22:52:43 +0530

    Update sms_sender.py

--- Computer-Vision/Face Detection/sms_sender.py
+++ Computer-Vision/Face Detection/sms_sender.py
@@ -11,14 +11,14 @@ from twilio.rest import Client
 import datetime
 date = datetime.datetime.now() 
 def sendsms():
-    account_sid = "ACdcfae8e5c4069d3cb80385ce2d36c855"
-    auth_token = "ccef4133311775065451e4b028c7d296"
+    account_sid = "#"
+    auth_token = "#"
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
                          body=f"Face detected at {date}",
-                         from_='+12407248231',
-                         to='+91 7350365576'
+                         from_='#',
+                         to='#'
                      )
 
     print(f"Messaged successfully !!")
